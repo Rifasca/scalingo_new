e7d10caa43efdf13e52dcca4200fb41df48fc8da
Jidu Lal <jidulalvm28@gmail.com>
2018-09-06 09:33:51 +0300

    changed

--- index.php
+++ index.php
@@ -1,34 +1,50 @@
 <?php
-	require 'vendor/autoload.php';
+	/* require 'vendor/autoload.php';
 	$url = parse_url(getenv('SCALINGO_INFLUX_URL'));
+	$client = new InfluxDB\Client($url['host'], $url['port']); */
 	
-	//echo "<pre>"; print_r($url); echo "</pre><br>";
-	//echo "<pre>"; print_r($url['host']); echo "</pre><br>";
-	
-	$client = new InfluxDB\Client($url['host'], $url['port']);
-	echo "<pre>"; print_r($client); echo "</pre><br>";
-	
-	// list databases
-	// show a list of all users
-	$results = $client->admin->showUsers();
 
-	// show users returns a ResultSet object
-	$users = $results->getPoints();
-	echo "<pre>"; print_r($users); echo "</pre><br>";
-	// select the database
-	//$database = $client->selectDB('influx_test_db');
-
-	// create the database with a retention policy
-	//$result = $database->create(new RetentionPolicy('test', '5d', 1, true));
-	//echo "database created 1";
-	
-	// check if a database exists then create it if it doesn't
-	//$database = $client->selectDB('test_db');
-	//echo "database seleted";
-	
-	//if (!$database->exists()) {
-		//$database->create(new RetentionPolicy('test', '1d', 2, true));
-		
-		//echo "database created 2";
-	//}
+	require 'vendor/autoload.php';
+	// vagrant ip
+	$host = '192.168.33.10';
+	function randFloat($min, $max)
+	{
+		$range = $max-$min;
+		$num = $min + $range * mt_rand(0, 32767)/32767;
+		$num = round($num, 4);
+		return ((float) $num);
+	}
+	$client = new \InfluxDB\Client($host);
+	$database = $client->selectDB('test');
+	if ($database->exists()) {
+		$database->drop();
+	}
+	$database->create(new \InfluxDB\Database\RetentionPolicy('test', '12w', 1, true));
+	$start = microtime(true);
+	$countries = ['nl', 'gb', 'us', 'be', 'th', 'jp', 'nl', 'sa'];
+	$colors = ['orange', 'black', 'yellow', 'white', 'red', 'purple'];
+	$points = [];
+	for ($i=0; $i < 1000; $i++) {
+		$points[] = new \InfluxDB\Point(
+			'flags',
+			randFloat(1, 999),
+			['country' => $countries[array_rand($countries)]],
+			['color' => $colors[array_rand($colors)]],
+			(int) shell_exec('date +%s%N')+mt_rand(1,1000)
+		);
+	};
+	// insert the points
+	$database->writePoints($points);
+	$end = microtime(true);
+	$country = $countries[array_rand($countries)];
+	$color = $colors[array_rand($colors)];
+	$results = $database->query("SELECT * FROM flags WHERE country = '$country' LIMIT 5")->getPoints();
+	$results2 = $database->query("SELECT * FROM flags WHERE color = '$color' LIMIT 5")->getPoints();
+	echo "Showing top 5 flags from country $country:" . PHP_EOL;
+	print_r($results);
+	echo PHP_EOL;
+	echo "Showing top 5 flags with color $color:" . PHP_EOL;
+	print_r($results2);
+	echo PHP_EOL;
+	echo sprintf('Executed 1000 inserts in %.2f seconds', $end - $start);
 ?>
