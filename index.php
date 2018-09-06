c0b965dc0afdec026543df5390795b06ca9fcae4
Jidu Lal <jidulalvm28@gmail.com>
2018-09-06 09:09:41 +0300

    Vendor added

--- index.php
+++ index.php
@@ -7,4 +7,19 @@
 	
 	$client = new InfluxDB\Client($url['host'], $url['port']);
 	echo "<pre>"; print_r($client); echo "</pre><br>";
+	
+	
+	// create the database with a retention policy
+	$result = $database->create(new RetentionPolicy('test', '5d', 1, true));
+	echo "database created 1";
+	
+	// check if a database exists then create it if it doesn't
+	$database = $client->selectDB('test_db');
+	echo "database seleted";
+	
+	if (!$database->exists()) {
+		$database->create(new RetentionPolicy('test', '1d', 2, true));
+		
+		echo "database created 2";
+	}
 ?>
