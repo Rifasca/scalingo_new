1aa0db8196e1c56fb453d5fb843995a447cb63f5
Jidu Lal <jidulalvm28@gmail.com>
2018-09-06 08:59:26 +0300

    Vendor added

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
+	require 'vendor/autoload.php';
 	$url = parse_url(getenv('SCALINGO_INFLUX_URL'));
 	
-	echo "<pre>"; print_r($url); echo "</pre>";
+	echo "<pre>"; print_r($url); echo "</pre><br>";
+	echo "<pre>"; print_r($url['host']); echo "</pre><br>";
+	
+	$client = new InfluxDB\Client($url['host'], $url['port']);
+	echo "<pre>"; print_r($client); echo "</pre><br>";
 ?>
